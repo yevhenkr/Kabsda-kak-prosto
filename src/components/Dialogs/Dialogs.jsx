996cb05253effe36aef03859ccbf396ac463005e
Eugen <vasilenkoevgen88@gmail.com>
2023-05-23 13:14:51 +0300

    Ref Dialoges

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,24 +3,40 @@ import s from './Dialogs.module.css'
 import { Link } from "react-router-dom";
 import { useLocation } from 'react-router-dom';
 
+const Dialog = (props) => {
+  const location = useLocation();
+  return <div>
+    <Link to={{ pathname: `${location.pathname}/${props.id}` }} className={props.className}>
+      {props.name}
+    </Link>
+  </div>
+}
 
+const Message = (props) => {
+  return <div>
+    {props.name}
+  </div>
+}
 
 const Dialogs = (props) => {
-  const location = useLocation();
+  // const location = useLocation();
   return <div className={s.diloges}>
     <div className={s.dilogesItems}>
-      <Link to={{ pathname: `${location.pathname}/1` }} className={`${s.dialoguesItem} ${s.active}`}>
-        Kirill
-      </Link>
-      <div className={s.dilogesItem}>Viky</div>
-      <div className={s.dilogesItem}>Taras</div>
-      <div className={s.dilogesItem}>Nastiy</div>
-      <div className={s.dilogesItem}>Oleg</div>
+      <Dialog name='Kirill' id='1' className={`${s.dialoguesItem} ${s.active}`} />
+      <Dialog name='Viky' id='2' className={s.dilogesItem} />
+      <Dialog name='Taras' id='3' className={s.dilogesItem} />
+      <Dialog name='Nastiy' id='4' className={s.dilogesItem} />
+      <Dialog name='Oleg' id='5' className={s.dilogesItem} />
     </div>
     <div className={s.dilogesMesseges}>
-      <div className={s.messeg}>Hi</div>
-      <div className={s.messeg}></div> Answer</div>
-    <div className={s.messeg}>Yo</div>
+      <Message name='Hi' />
+      <Message name='Answer' />
+      <Message name='Yo' />
+      {/* <div className={s.messeg}>Hi</div>
+      <div className={s.messeg}> Answer</div>
+      <div className={s.messeg}>Yo</div> */}
+    </div>
   </div>
+
 }
 export default Dialogs;
