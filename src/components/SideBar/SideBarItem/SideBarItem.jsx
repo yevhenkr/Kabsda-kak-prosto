b632a82d1f32f4767c91d68b9ec021255c95dbeb
Eugen <vasilenkoevgen88@gmail.com>
2023-05-25 10:40:56 +0300

    ref

--- src/components/SideBar/SideBarItem/SideBarItem.jsx
+++ src/components/SideBar/SideBarItem/SideBarItem.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import classas from './SideBarItem.module.css'
 
 const SideBarItem = (props) => {
-    debugger;
     return <div>
         <img className={classas.icon} src={props.icon} alt="" />
         <div><span className={classas.sideBarItem}>{props.name}</span></div>
