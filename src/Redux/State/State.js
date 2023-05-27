e556779242770e44e90d21fc12fc0904c83ba1f4
Eugen <vasilenkoevgen88@gmail.com>
2023-05-27 17:11:14 +0300

    Ref

--- src/Redux/State/State.js
+++ src/Redux/State/State.js
@@ -2,28 +2,28 @@ let store = {
     _state: {
         profilePage: {
             posts: [
-                {id: 1, message: 'Hi, how are you?', likesCount: 12},
-                {id: 2, message: 'It\'s my first post', likesCount: 11},
-                {id: 3, message: 'Blabla', likesCount: 11},
-                {id: 4, message: 'Dada', likesCount: 11}
+                { id: 1, message: 'Hi, how are you?', likesCount: 12 },
+                { id: 2, message: 'It\'s my first post', likesCount: 11 },
+                { id: 3, message: 'Blabla', likesCount: 11 },
+                { id: 4, message: 'Dada', likesCount: 11 }
             ],
             newPostText: 'it-kamasutra.com'
         },
         dialogsPage: {
             dialogs: [
-                {id: 1, name: 'Dimych'},
-                {id: 2, name: 'Andrew'},
-                {id: 3, name: 'Sveta'},
-                {id: 4, name: 'Sasha'},
-                {id: 5, name: 'Viktor'},
-                {id: 6, name: 'Valera'}
+                { id: 1, name: 'Dimych' },
+                { id: 2, name: 'Andrew' },
+                { id: 3, name: 'Sveta' },
+                { id: 4, name: 'Sasha' },
+                { id: 5, name: 'Viktor' },
+                { id: 6, name: 'Valera' }
             ],
             messages: [
-                {id: 1, message: 'Hi'},
-                {id: 2, message: 'How is your it-kamasutra?'},
-                {id: 3, message: 'Yo'},
-                {id: 4, message: 'Yo'},
-                {id: 5, message: 'Yo'}
+                { id: 1, message: 'Hi' },
+                { id: 2, message: 'How is your it-kamasutra?' },
+                { id: 3, message: 'Yo' },
+                { id: 4, message: 'Yo' },
+                { id: 5, message: 'Yo' }
             ]
         },
         sideBar: {
