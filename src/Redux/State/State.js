fc6f32c4771e55f21707339f3f91ca55fb95feac
Eugen <vasilenkoevgen88@gmail.com>
2023-05-27 17:11:03 +0300

    dispatch и action

--- src/Redux/State/State.js
+++ src/Redux/State/State.js
@@ -2,28 +2,28 @@ let store = {
     _state: {
         profilePage: {
             posts: [
-                { id: 1, message: 'Hi, how are you?', likesCount: 12 },
-                { id: 2, message: 'It\'s my first post', likesCount: 11 },
-                { id: 3, message: 'Blabla', likesCount: 11 },
-                { id: 4, message: 'Dada', likesCount: 11 }
+                {id: 1, message: 'Hi, how are you?', likesCount: 12},
+                {id: 2, message: 'It\'s my first post', likesCount: 11},
+                {id: 3, message: 'Blabla', likesCount: 11},
+                {id: 4, message: 'Dada', likesCount: 11}
             ],
             newPostText: 'it-kamasutra.com'
         },
         dialogsPage: {
             dialogs: [
-                { id: 1, name: 'Dimych' },
-                { id: 2, name: 'Andrew' },
-                { id: 3, name: 'Sveta' },
-                { id: 4, name: 'Sasha' },
-                { id: 5, name: 'Viktor' },
-                { id: 6, name: 'Valera' }
+                {id: 1, name: 'Dimych'},
+                {id: 2, name: 'Andrew'},
+                {id: 3, name: 'Sveta'},
+                {id: 4, name: 'Sasha'},
+                {id: 5, name: 'Viktor'},
+                {id: 6, name: 'Valera'}
             ],
             messages: [
-                { id: 1, message: 'Hi' },
-                { id: 2, message: 'How is your it-kamasutra?' },
-                { id: 3, message: 'Yo' },
-                { id: 4, message: 'Yo' },
-                { id: 5, message: 'Yo' }
+                {id: 1, message: 'Hi'},
+                {id: 2, message: 'How is your it-kamasutra?'},
+                {id: 3, message: 'Yo'},
+                {id: 4, message: 'Yo'},
+                {id: 5, message: 'Yo'}
             ]
         },
         sideBar: {
@@ -54,7 +54,7 @@ let store = {
             this._state.profilePage.posts.push(newPost);
             this._state.profilePage.newPostText = '';
             this._callSubscriber(this._state);
-        } else if(action.type = "UPDATE-NEW-POST-TEXT") {
+        } else if (action.type = "UPDATE-NEW-POST-TEXT") {
             this._state.profilePage.newPostText = action.text;
             this._callSubscriber(this._state);
         }
