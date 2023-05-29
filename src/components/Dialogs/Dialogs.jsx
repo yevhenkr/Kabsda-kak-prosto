836a4d047dce0ebcdff6d35f953f44ef18516de9
Eugen <vasilenkoevgen88@gmail.com>
2023-05-29 15:08:05 +0300

    Fix

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -7,7 +7,7 @@ import { updateNewMessageBodyCreator, sendMessageCreator } from '../../Redux/Sta
 
 const Dialogs = (props) => {
     let state = props.store.getState().dialogsPage;
-    // debugger;
+    debugger;
     let dialogsElements = state.dialogs.map(d => <DialogItem name={d.name} id={d.id} />);
     let messagesElements = state.messages.map(m => <Message message={m.message} />);
     let newMessageBody = state.newMessageBody;
