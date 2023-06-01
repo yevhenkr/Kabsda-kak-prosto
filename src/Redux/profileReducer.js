804316edc007b95efb6a1473b50ecf8b9f4550d3
Eugen <vasilenkoevgen88@gmail.com>
2023-06-01 13:29:35 +0300

    fix mutebl

--- src/Redux/profileReducer.js
+++ src/Redux/profileReducer.js
@@ -1,37 +1,40 @@
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
-const InitState = {
-    posts: [
-        { id: 1, message: 'Hi, how are you?', likesCount: 12 },
-        { id: 2, message: 'It\'s my first post', likesCount: 11 },
-        { id: 3, message: 'Blabla', likesCount: 11 },
-        { id: 4, message: 'Dada', likesCount: 11 }
-    ],
-    newPostText: ''
-}
-const profileReducer = (state = InitState, action) => {
-    //    debugger
-    switch (action.type) {
-        case ADD_POST:
-            let newPost = {
-                id: 5,
-                message: state.newPostText,
-                likesCount: 0
-            };
-            state.posts.push(newPost);
-            state.newPostText = '';
-            return state;
-        case UPDATE_NEW_POST_TEXT:
-            state.newPostText = action.newText;
-            return state;
-        default:
-            return state;
-    }
-}
+const initialState = {
+  posts: [
+    { id: 1, message: 'Hi, how are you?', likesCount: 12 },
+    { id: 2, message: 'It\'s my first post', likesCount: 11 },
+    { id: 3, message: 'Blabla', likesCount: 11 },
+    { id: 4, message: 'Dada', likesCount: 11 }
+  ],
+  newPostText: ''
+};
 
-export const addPostActionCreator = () => ({ type: ADD_POST })
-export const updateNewPostTextActionCreator = (text) =>
-    ({ type: UPDATE_NEW_POST_TEXT, newText: text })
+const profileReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_POST:
+      const newPost = {
+        id: 5,
+        message: state.newPostText,
+        likesCount: 0
+      };
+      return {
+        ...state,
+        posts: [...state.posts, newPost],
+        newPostText: ''
+      };
+    case UPDATE_NEW_POST_TEXT:
+      return {
+        ...state,
+        newPostText: action.newText
+      };
+    default:
+      return state;
+  }
+};
+
+export const addPostActionCreator = () => ({ type: ADD_POST });
+export const updateNewPostTextActionCreator = (text) => ({ type: UPDATE_NEW_POST_TEXT, newText: text });
 
 export default profileReducer;
