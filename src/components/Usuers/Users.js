f3215f6997eea0fd8eff3a9f02773cc35ab99336
Eugen <vasilenkoevgen88@gmail.com>
2023-06-06 14:45:03 +0300

    United create new CleanFuncComponent User. ApiComponent ContainerComponent

---
+++ src/components/Usuers/Users.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import classes from './Users.module.css';
+import userPhoto from '../../../src/assets/imges/users.jpg'
+
+
+const Users = (props) => {
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
+    }
+    return <div>
+        {pages.map(p => {
+            return <span onClick={(e) => { props.onPageChanged(p) }} className={props.currentPage === p && classes.fontBold}>{p}</span>
+        })
+        }
+        {
+            props.users.map(u => <div key={u.id}>
+                <span>
+                    <div>
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={classes.userPhoto} alt="dd"/>
+                    </div>
+                    <div>
+                        {u.followed
+                            ? <button onClick={() => {
+                                props.unfollow(u.id)
+                            }}>Unfollow</button>
+                            : <button onClick={() => {
+                                props.follow(u.id)
+                            }}>Follow</button>}
+
+                    </div>
+                </span>
+                <span>
+                    <span>
+                        <div>{u.name}</div>
+                        <div>{u.status}</div>
+                    </span>
+                    <span>
+                        {/* <div>{u.location.country}</div>
+                        <div>{u.location.city}</div> */}
+                    </span>
+                </span>
+            </div>)
+        }
+    </div>
+}
+
+export default Users;
--- src/components/Usuers/UsersContainer.js
+++ src/components/Usuers/UsersContainer.js
@@ -1,7 +1,37 @@
 import React from 'react';
-import Users from "./Users";
-import {connect} from "react-redux";
-import {followAC, setCurrentPageAC, setUsersAC, unfollowAC} from "../../Redux/usersReducer";
+// import UsersApiComponent from "./UsersApiComponent";
+import { connect } from "react-redux";
+import { followAC, setCurrentPageAC, setUsersAC, unfollowAC } from "../../Redux/usersReducer";
+import axios from 'axios';
+import Users from './Users';
+
+
+class UsersApiComponent extends React.Component {
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items);
+            });
+    }
+    onPageChanged = (currentPage) => {
+        this.props.setCurrentPage(currentPage);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items);
+            });
+    }
+    render() {
+
+        return <Users totalUsersCount={this.props.totalUsersCount}
+            pageSize={this.props.pageSize}
+            currentPage={this.props.currentPage}
+            onPageChanged={this.onPageChanged}
+            users={this.props.users}
+            follow={this.props.follow}
+            unfollow={this.props.unfollow}
+        />
+    }
+}
 
 let mapStateToProps = (state) => {
     return {
@@ -29,4 +59,4 @@ let mapDispatchToProps = (dispatch) => {
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Users);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersApiComponent);
