203fa0ceb1a6308a7150f0a89d5b304a97ebe9f8
Eugen <vasilenkoevgen88@gmail.com>
2023-06-09 11:16:02 +0300

    follow-unfollow api

--- src/components/Usuers/Users.js
+++ src/components/Usuers/Users.js
@@ -2,6 +2,7 @@ import React from 'react';
 import classes from './Users.module.css';
 import userPhoto from '../../../src/assets/imges/users.jpg'
 import { NavLink } from 'react-router-dom';
+import axios from 'axios';
 
 
 const Users = (props) => {
@@ -24,10 +25,28 @@ const Users = (props) => {
                     <div>
                         {u.followed
                             ? <button onClick={() => {
-                                props.unfollow(u.id)
+                                axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, { 
+                                    withCredentials: true,
+                                    headers: {
+                                        "API-KEY": "891717e9-a501-4f9b-8508-4b240ff784b6"
+                                    }}, )
+                                    .then(response => {
+                                        if (response.data.resultCode === 0) {
+                                            props.unfollow(u.id)
+                                        }
+                                    });
                             }}>Unfollow</button>
                             : <button onClick={() => {
-                                props.follow(u.id)
+                                axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`,{}, { 
+                                    withCredentials: true,
+                                    headers: {
+                                        "API-KEY": "891717e9-a501-4f9b-8508-4b240ff784b6"
+                                    }}, )
+                                    .then(response => {
+                                        if (response.data.resultCode === 0) {
+                                            props.follow(u.id)
+                                        }
+                                    });
                             }}>Follow</button>}
 
                     </div>
--- src/components/Usuers/UsersContainer.js
+++ src/components/Usuers/UsersContainer.js
@@ -9,7 +9,10 @@ import Preloader from '../../components/common/preloader/Preloader';
 class UsersContainer extends React.Component {
     componentDidMount() {
         this.props.toggleIsFetching(true);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`,
+            {
+                withCredentials: true
+            })
             .then(response => {
                 this.props.toggleIsFetching(false);
                 this.props.setUsers(response.data.items);
@@ -19,7 +22,10 @@ class UsersContainer extends React.Component {
     onPageChanged = (currentPage) => {
         this.props.setCurrentPage(currentPage);
         this.props.toggleIsFetching(true);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${this.props.pageSize}`)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${this.props.pageSize}`,
+            {
+                withCredentials: true
+            })
             .then(response => {
                 this.props.toggleIsFetching(false);
                 this.props.setUsers(response.data.items);
@@ -75,6 +81,6 @@ let mapStateToProps = (state) => {
 //     }
 // }
 
-export default connect(mapStateToProps, 
-    {follow, unfollow, setUsers, setCurrentPage, setTotalUsersCount, toggleIsFetching}
+export default connect(mapStateToProps,
+    { follow, unfollow, setUsers, setCurrentPage, setTotalUsersCount, toggleIsFetching }
 )(UsersContainer);
