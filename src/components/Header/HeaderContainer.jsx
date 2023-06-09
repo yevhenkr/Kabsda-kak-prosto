d6829dfeeb22b62b3cd827f6ce2ac599a8dc7b03
Eugen <vasilenkoevgen88@gmail.com>
2023-06-09 09:52:40 +0300

    auth/me ашч

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -7,8 +7,11 @@ class HeaderContainer extends React.Component {
   render() {
   axios.get(`https://social-network.samuraijs.com/api/1.0//auth/me`, {withCredentials: true})
   .then(response => {
-    // debugger
+    if (response.data.resultCode === 0) {
       this.props.setAuthUserData(response.data.data);
+      // let {id, login, email} = response.data.data; // Или єти две строки
+      // this.props.setAuthUserData(id, email, login);
+    }
   });
     return (
       <Header {...this.props}/>
